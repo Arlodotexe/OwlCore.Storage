349973ba5f764429fac5ee62f8f183ad1dd15b76
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-08-29 22:04:00 -0500

    Added StorableType

---
+++ src/StorableType.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OwlCore.Storage;
+
+/// <summary>
+/// A flag enum that indicates a certain type of storable resource.
+/// </summary>
+[Flags]
+public enum StorableType : byte
+{
+    /// <summary>
+    /// Indicates no storable type.
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    /// Indicates a storable file.
+    /// </summary>
+    File = 1,
+
+    /// <summary>
+    /// Indicates a storable folder.
+    /// </summary>
+    Folder = 2,
+
+    /// <summary>
+    /// Indicates all storable types.
+    /// </summary>
+    All = byte.MaxValue,
+}
