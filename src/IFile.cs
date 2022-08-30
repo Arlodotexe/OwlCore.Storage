12215e059cf5b714c4f6aa7f33cb664d56a7795b
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-08-29 22:03:37 -0500

    Added IFile

---
+++ src/IFile.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OwlCore.Storage;
+
+/// <summary>
+/// The minimal functional requirements for a file.
+/// </summary>
+public interface IFile : IStorable
+{
+    /// <summary>
+    /// Opens a new stream to the file.
+    /// </summary>
+    public Task<Stream> OpenStreamAsync(FileAccess accessMode = FileAccess.Read, CancellationToken cancellationToken = default);
+}
