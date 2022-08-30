81a3511411409f0c4a39e2e0d7bc0304d3135a1c
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-08-29 21:03:54 -0500

    Initial commit

---
+++ src/IStorable.cs
@@ -0,0 +1,19 @@
+﻿namespace OwlCore.Storage;
+
+public interface IStorable
+{
+    /// <summary>
+    /// A unique and consistent identifier for this file or folder.
+    /// </summary>
+    /// <remarks>
+    /// Not all files are addressable by path.
+    /// Uri paths (like OneDrive) can change entirely when re-authenticating.
+    /// Therefore, we need a dedicated resource identifier.
+    /// </remarks>
+    public string Id { get; }
+
+    /// <summary>
+    /// The name of the file or folder, with the extension (if any).
+    /// </summary>
+    public string Name { get; } 
+}
