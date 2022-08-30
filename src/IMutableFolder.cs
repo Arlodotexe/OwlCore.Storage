f3035a4019147e962e1fe5cc92bc58ef7ec1e591
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-08-30 00:43:28 -0500

    Added IMutableFolder

---
+++ src/IMutableFolder.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace OwlCore.Storage;
+
+/// <summary>
+/// Represents a folder whose content can change.
+/// </summary>
+public interface IMutableFolder : IFolder
+{
+    /// <summary>
+    /// Asynchronously retrieves a disposable object which can notify of changes to the folder.
+    /// </summary>
+    /// <returns>A Task representing the asynchronous operation. The result is a disposable object which can notify of changes to the folder.</returns>
+    public Task<IFolderWatcher> GetFolderWatcherAsync(CancellationToken cancellationToken = default);
+}
