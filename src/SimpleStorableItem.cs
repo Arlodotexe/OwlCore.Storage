14ab11d7212ee0981750fbcb944536755cf4538a
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-08-30 14:53:59 -0500

    Added a minimum implementation of IStorable.

---
+++ src/SimpleStorableItem.cs
@@ -0,0 +1,27 @@
+﻿namespace OwlCore.Storage;
+
+/// <summary>
+/// A minimum implementation of <see cref="IStorable"/>.
+/// </summary>
+/// <remarks>
+/// Useful to identify a resource which might not be accessible, such as when it's removed.
+/// </remarks>
+public sealed class SimpleStorableItem : IStorable
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="SimpleStorableItem"/>.
+    /// </summary>
+    /// <param name="id">A unique and consistent identifier for this file or folder. This dedicated resource identifier is used to identify the exact file you're pointing to.</param>
+    /// <param name="name">The name of the file or folder, with the extension (if any).</param>
+    public SimpleStorableItem(string id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    /// <inheritdoc />
+    public string Id { get; }
+
+    /// <inheritdoc />
+    public string Name { get; }
+}
