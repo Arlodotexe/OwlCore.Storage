ea2ea349b35c1f42ce84aca5f9a45feb7d560e68
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-08-29 22:05:01 -0500

    Added IFolder

---
+++ src/IFolder.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace OwlCore.Storage;
+
+/// <summary>
+/// The minimal functional requirements for a folder.
+/// </summary>
+public interface IFolder : IStorable
+{
+    /// <summary>
+    /// Retrieves the folders in this directory.
+    /// </summary>
+    public IAsyncEnumerable<IStorable> GetItemsAsync(StorableType type = StorableType.All, CancellationToken cancellationToken = default);
+}
