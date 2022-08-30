ae754c79cc8c55de614520cf3c6b2f54426eb7b5
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-08-29 23:43:16 -0500

    Added IStorageProperties{T}

---
+++ src/IStorageProperties.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OwlCore.Storage;
+
+/// <summary>
+/// The recommended pattern for file properties is create an interface with an async method that returns <c>IStorageProperties{YourPropertyClass}</c>.
+/// </summary>
+/// <typeparam name="T">The type used to store information about properties.</typeparam>
+public interface IStorageProperties<T> : IDisposable
+{
+    /// <summary>
+    /// Gets the current property value.
+    /// </summary>
+    public T Value { get; }
+
+    /// <summary>
+    /// Raised when the <see cref="Value"/> is updated.
+    /// </summary>
+    public event EventHandler<T> ValueUpdated;
+}
