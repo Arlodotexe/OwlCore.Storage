d2d3896b26bb667d5e60be03245b6cb02bb4c344
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-08-30 00:39:55 -0500

    Added IFolderWatcher

---
+++ src/IFolderWatcher.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Specialized;
+
+namespace OwlCore.Storage;
+
+/// <summary>
+/// A disposable object which can notify of changes to the folder.
+/// </summary>
+public interface IFolderWatcher : INotifyCollectionChanged, IDisposable, IAsyncDisposable
+{
+}
