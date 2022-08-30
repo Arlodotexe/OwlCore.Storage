f504f732d9fef7fbe882a6c4dd5ae71d17c7ff47
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-08-30 01:09:15 -0500

    Added GetItemAsync folder extension method, and IFolderCanFastGetItem interface for fast paths.

--- src/Extensions/FolderExtensions.cs
+++ src/Extensions/FolderExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace OwlCore.Storage;
 
@@ -24,5 +26,25 @@ public static class FolderExtensions
     /// <param name="cancellationToken">The cancellation token to observe.</param>
     /// <returns></returns>
     public static IAsyncEnumerable<IFolder> GetFoldersAsync(this IFolder folder, CancellationToken cancellationToken = default) => folder.GetItemsAsync(StorableType.Folder, cancellationToken).Cast<IFolder>();
-}
 
+
+    /// <summary>
+    /// Retrieves the <see cref="IStorable"/> item which has the provided <paramref name="id"/>.
+    /// </summary>
+    /// <param name="folder">The folder to get items from.</param>
+    /// <param name="id">The <see cref="IStorable.Id"/> of the storable item to retrieve.</param>
+    /// <param name="cancellationToken">The cancellation token to observe.</param>
+    /// <returns>The relevant</returns>
+    /// <exception cref="FileNotFoundException">The item was not found in the provided folder.</exception>
+    public static async Task<IStorable> GetItemAsync(this IFolder folder, string id, CancellationToken cancellationToken = default)
+    {
+        if (folder is IFolderCanFastGetItem fastPath)
+            return await fastPath.GetItemAsync(id, cancellationToken);
+
+        var targetItem = await folder.GetItemsAsync(cancellationToken: cancellationToken).FirstOrDefaultAsync(x => x.Id == id, cancellationToken: cancellationToken);
+        if (targetItem is null)
+            throw new FileNotFoundException($"No storage item with the ID \"{id}\" could be found.");
+
+        return targetItem;
+    }
+}
---
+++ src/Extensions/IFolderCanFastGetItem.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OwlCore.Storage;
+
+/// <summary>
+/// Provides a fast-path for the <see cref="FolderExtensions.GetItemAsync"/> extension method.
+/// </summary>
+/// <exception cref="FileNotFoundException">The item was not found in the provided folder.</exception>
+public interface IFolderCanFastGetItem : IFolder
+{
+    /// <summary>
+    /// Retrieves the <see cref="IStorable"/> item which has the provided <paramref name="id"/>.
+    /// </summary>
+    /// <param name="id">The <see cref="IStorable.Id"/> of the storable item to retrieve.</param>
+    /// <param name="cancellationToken">The cancellation token to observe.</param>
+    /// <returns></returns>
+    public Task<IStorable> GetItemAsync(string id, CancellationToken cancellationToken = default);
+}
