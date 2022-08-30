0aa041750a656c4b7c94de9ead410850c33baaf6
Arlo Godfrey <arlo.godfrey@outlook.com>
2022-08-29 22:03:18 -0500

    Added IStorable

--- src/IStorable.cs
+++ src/IStorable.cs
@@ -1,14 +1,19 @@
 ﻿namespace OwlCore.Storage;
 
+/// <summary>
+/// The absolute minimum requirements for all storable items.
+/// </summary>
 public interface IStorable
 {
     /// <summary>
-    /// A unique and consistent identifier for this file or folder.
+    /// A unique and consistent identifier for this file or folder. This dedicated resource identifier is used to identify the exact file you're pointing to.
     /// </summary>
     /// <remarks>
-    /// Not all files are addressable by path.
-    /// Uri paths (like OneDrive) can change entirely when re-authenticating.
-    /// Therefore, we need a dedicated resource identifier.
+    /// Uri paths, especially those from cloud storage, can change regularly (e.g. when re-authenticating), and some files/folders aren't even addressable, meaning paths can't be used as a reliable content identifier.
+    /// Also, custom and especially cloud file systems often use a predictable or custom ID as the primary-key in a flat or near-flat database table of files and folders. This also means that names aren't guaranteed to be unique within a folder.
+    ///
+    /// <para />
+    /// Instead, since the underlying implementation knows the requirements, it can supply what data it has as long as it uniquely and consistently identifies the content.
     /// </remarks>
     public string Id { get; }
 
