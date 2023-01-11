4c8bdea04fcf178a6b297b78963fba45025c6023
Yoshi Askharoun <jjask7@gmail.com>
2023-01-11 00:53:20 -0600

    Added tests for ZIP archives created in memory

---
+++ tests/OwlCore.Storage.Tests/SystemIO/Compression/InMemIFileTests.cs
@@ -0,0 +1,39 @@
+﻿using OwlCore.Storage.CommonTests;
+using OwlCore.Storage.SystemIO.Compression;
+using System.IO.Compression;
+
+namespace OwlCore.Storage.Tests.SystemIO.Compression;
+
+/// <summary>
+/// A test suite for <see cref="ZipArchiveEntryFile"/>s created entirely in memory.
+/// </summary>
+[TestClass]
+public class InMemIFileTests : CommonIFileTests
+{
+    // Required for base class to perform common tests.
+    public override Task<IFile> CreateFileAsync()
+    {
+        MemoryStream archiveStream = new();
+        ZipArchive archive = new(archiveStream, ZipArchiveMode.Update);
+        var entry = archive.CreateEntry($"{Guid.NewGuid()}");
+
+        using (var entryStream = entry.Open())
+        {
+            var randomData = GenerateRandomData(256_000);
+            entryStream.Write(randomData);
+        }
+
+        var file = new ZipArchiveEntryFile(entry, new ZipArchiveFolder($"{Guid.NewGuid()}", $"{Guid.NewGuid()}", archive));
+
+        return Task.FromResult<IFile>(file);
+
+        static byte[] GenerateRandomData(int length)
+        {
+            var rand = new Random();
+            var b = new byte[length];
+            rand.NextBytes(b);
+
+            return b;
+        }
+    }
+}
---
+++ tests/OwlCore.Storage.Tests/SystemIO/Compression/InMemIFolderTests.cs
@@ -0,0 +1,41 @@
+﻿using OwlCore.Storage.CommonTests;
+using OwlCore.Storage.SystemIO.Compression;
+using System.IO.Compression;
+
+namespace OwlCore.Storage.Tests.SystemIO.Compression;
+
+/// <summary>
+/// A test suite for <see cref="ZipArchiveFolder"/>s created entirely in memory.
+/// </summary>
+[TestClass]
+public class InMemIFolderTests : IModifiableFolderTests
+{
+    // Required for base class to perform common tests.
+    public override Task<IModifiableFolder> CreateModifiableFolderAsync()
+    {
+        MemoryStream archiveStream = new();
+        ZipArchive archive = new(archiveStream, ZipArchiveMode.Update);
+
+        return Task.FromResult<IModifiableFolder>(new ZipArchiveFolder($"{Guid.NewGuid()}", $"{Guid.NewGuid()}", archive));
+    }
+
+    public override async Task<IModifiableFolder> CreateModifiableFolderWithItems(int fileCount, int folderCount)
+    {
+        MemoryStream archiveStream = new();
+        ZipArchive archive = new(archiveStream, ZipArchiveMode.Update);
+
+        var folder = new ZipArchiveFolder($"{Guid.NewGuid()}", $"{Guid.NewGuid()}", archive);
+
+        for (int i = 0; i < fileCount; i++)
+        {
+            await folder.CreateFileAsync($"{Guid.NewGuid()}");
+        }
+
+        for (int i = 0; i < folderCount; i++)
+        {
+            await folder.CreateFolderAsync($"{Guid.NewGuid()}");
+        }
+
+        return folder;
+    }
+}
