c1aef7ef42c831f32b94c77ddbabe00d2f83fc38
Yoshi Askharoun <jjask7@gmail.com>
2023-01-11 02:23:46 -0600

    Added tests for ZipArchiveFolders on disk

---
+++ tests/OwlCore.Storage.Tests/SystemIO/Compression/IFolderTests.cs
@@ -0,0 +1,46 @@
+﻿using OwlCore.Storage.CommonTests;
+using OwlCore.Storage.SystemIO.Compression;
+using System.IO.Compression;
+
+namespace OwlCore.Storage.Tests.SystemIO.Compression;
+
+[TestClass]
+public class IFolderTests : IModifiableFolderTests
+{
+    // Required for base class to perform common tests.
+    public override Task<IModifiableFolder> CreateModifiableFolderAsync()
+    {
+        ZipArchive archive = ZipFile.Open(CreateEmptyArchiveOnDisk(), ZipArchiveMode.Update);
+
+        return Task.FromResult<IModifiableFolder>(new ZipArchiveFolder($"{Guid.NewGuid()}", $"{Guid.NewGuid()}", archive));
+    }
+
+    public override async Task<IModifiableFolder> CreateModifiableFolderWithItems(int fileCount, int folderCount)
+    {
+        var folder = await CreateModifiableFolderAsync();
+
+        for (int i = 0; i < fileCount; i++)
+        {
+            await folder.CreateFileAsync($"{Guid.NewGuid()}");
+        }
+
+        for (int i = 0; i < folderCount; i++)
+        {
+            await folder.CreateFolderAsync($"{Guid.NewGuid()}");
+        }
+
+        return folder;
+    }
+
+    private static string CreateEmptyArchiveOnDisk()
+    {
+        // Create new archive on disk
+        string archiveId = $"archiveTest_{Guid.NewGuid()}";
+        var tempArchivePath = Path.Combine(Path.GetTempPath(), archiveId);
+
+        var dir = Directory.CreateDirectory(tempArchivePath);
+        ZipFile.CreateFromDirectory(tempArchivePath, tempArchivePath += ".zip");
+
+        return tempArchivePath;
+    }
+}
