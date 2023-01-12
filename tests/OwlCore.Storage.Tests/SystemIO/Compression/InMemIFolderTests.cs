1221cfabad781c936d02d000cd830548c9eb4fb7
Yoshi Askharoun <jjask7@gmail.com>
2023-01-11 22:33:53 -0600

    Added tests for nested folders in ZipArchiveFolders

--- tests/OwlCore.Storage.Tests/SystemIO/Compression/InMemIFolderTests.cs
+++ tests/OwlCore.Storage.Tests/SystemIO/Compression/InMemIFolderTests.cs
@@ -35,4 +35,120 @@ public class InMemIFolderTests : IModifiableFolderTests
 
         return folder;
     }
+
+    public async Task<IModifiableFolder> CreateModifiableFolderWithNestedItems()
+    {
+        // Create the following directory structure:
+        // root
+        //  ├ subA
+        //  │  └ fileB
+        //  ├ subB
+        //  │  ├ subC
+        //  │  │  └ fileC
+        //  │  └ fileB
+        //  └ fileRoot
+
+        var root = await CreateModifiableFolderAsync();
+        {
+            var subA = await root.CreateFolderAsync("subA") as IModifiableFolder;
+            {
+                Assert.IsNotNull(subA);
+
+                await subA.CreateFileAsync("fileA");
+            }
+
+            var subB = await root.CreateFolderAsync("subB") as IModifiableFolder;
+            {
+                Assert.IsNotNull(subB);
+
+                await subB.CreateFileAsync("fileB");
+
+                var subC = await subB.CreateFolderAsync("subC") as IModifiableFolder;
+                {
+                    Assert.IsNotNull(subC);
+
+                    await subC.CreateFileAsync("fileC");
+                }
+            }
+
+            await root.CreateFileAsync("fileRoot");
+        }
+
+        return root;
+    }
+
+    [TestMethod]
+    public async Task CreateNewFolderAsyncTest_FolderWithNestedItems()
+    {
+        var root = await CreateModifiableFolderWithNestedItems();
+
+        // Check each path
+        var fileRootEx = await root.GetItemAsync("fileRoot");
+        Assert.AreEqual("fileRoot", fileRootEx.Path);
+
+        var subAEx = await root.GetItemAsync("subA") as IAddressableFolder;
+        Assert.IsNotNull(subAEx);
+        Assert.AreEqual("subA/", subAEx.Path);
+
+        var fileAEx = await subAEx.GetItemAsync("fileA");
+        Assert.IsInstanceOfType<IFile>(fileAEx);
+        Assert.AreEqual("subA/fileA", fileAEx.Path);
+
+        var subBEx = await root.GetItemAsync("subB") as IAddressableFolder;
+        Assert.IsNotNull(subBEx);
+        Assert.AreEqual("subB/", subBEx.Path);
+
+        var fileBEx = await subBEx.GetItemAsync("fileB");
+        Assert.IsInstanceOfType<IFile>(fileBEx);
+        Assert.AreEqual("subB/fileB", fileBEx.Path);
+
+        var subCEx = await subBEx.GetItemAsync("subC") as IAddressableFolder;
+        Assert.IsNotNull(subCEx);
+        Assert.AreEqual("subB/subC/", subCEx.Path);
+
+        var fileCEx = await subCEx.GetItemAsync("fileC");
+        Assert.IsInstanceOfType<IFile>(fileCEx);
+        Assert.AreEqual("subB/subC/fileC", fileCEx.Path);
+    }
+
+    [TestMethod]
+    public async Task GetItemsAsyncText_FolderWithNestedItems()
+    {
+        var root = await CreateModifiableFolderWithNestedItems();
+
+        var subA = await root.GetItemAsync("subA") as IFolder;
+        {
+            Assert.IsNotNull(subA);
+
+            var subAItems = await subA.GetItemsAsync().ToListAsync();
+            Assert.AreEqual(1, subAItems.Count);
+
+            var fileA = subAItems[0];
+            Assert.IsInstanceOfType<IFile>(fileA);
+            Assert.AreEqual("fileA", fileA.Id);
+        }
+
+        var subB = await root.GetItemAsync("subB") as IFolder;
+        {
+            Assert.IsNotNull(subB);
+
+            var subBItems = await subB.GetItemsAsync().ToListAsync();
+            Assert.AreEqual(2, subBItems.Count);
+
+            var fileB = subBItems.First(i => i.Id == "fileB");
+            Assert.IsInstanceOfType<IFile>(fileB);
+
+            var subC = subBItems.First(i => i.Id == "subC") as IFolder;
+            {
+                Assert.IsNotNull(subC);
+
+                var subCItems = await subC.GetItemsAsync().ToListAsync();
+                Assert.AreEqual(1, subCItems.Count);
+
+                var fileC = subCItems[0];
+                Assert.IsInstanceOfType<IFile>(fileC);
+                Assert.AreEqual("fileC", fileC.Id);
+            }
+        }
+    }
 }
