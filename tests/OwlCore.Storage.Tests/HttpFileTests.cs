4fc181e1101206cfc6fc8c24ce2e621d3fee5321
Arlo Godfrey <arlo.godfrey@outlook.com>
2023-02-18 16:19:52 -0600

    Formatting

--- tests/OwlCore.Storage.Tests/HttpFileTests.cs
+++ tests/OwlCore.Storage.Tests/HttpFileTests.cs
@@ -1,47 +1,46 @@
 using OwlCore.Storage.CommonTests;
 
-namespace OwlCore.Storage.Tests
+namespace OwlCore.Storage.Tests;
+
+[TestClass]
+public class HttpFileTests : CommonIFileTests
 {
-    [TestClass]
-    public class HttpFileTests : CommonIFileTests
+    public override bool SupportsWriting => false;
+
+    // Required for base class to perform common tests.
+    public override Task<IFile> CreateFileAsync()
     {
-        public override bool SupportsWriting => false;
+        var file = new HttpFile(new Uri("https://example.com/"));
 
-        // Required for base class to perform common tests.
-        public override Task<IFile> CreateFileAsync()
-        {
-            var file = new HttpFile(new Uri("https://example.com/"));
+        return Task.FromResult<IFile>(file);
+    }
 
-            return Task.FromResult<IFile>(file);
-        }
+    [TestMethod]
+    public void NameShouldNotMatchUri()
+    {
+        var file = new HttpFile(new Uri("https://example.com/test.zip"));
 
-        [TestMethod]
-        public void NameShouldNotMatchUri()
-        {
-            var file = new HttpFile(new Uri("https://example.com/test.zip"));
+        Assert.AreNotEqual(file.Name, file.Uri.OriginalString);
+        Assert.IsTrue(file.Uri.OriginalString.Contains(file.Name));
+    }
 
-            Assert.AreNotEqual(file.Name, file.Uri.OriginalString);
-            Assert.IsTrue(file.Uri.OriginalString.Contains(file.Name));
-        }
+    [TestMethod]
+    public void FileNameOnSimpleUrl()
+    {
+        var file = new HttpFile(new Uri("https://example.com/test.zip"));
 
-        [TestMethod]
-        public void FileNameOnSimpleUrl()
-        {
-            var file = new HttpFile(new Uri("https://example.com/test.zip"));
+        Assert.AreNotEqual(file.Name, file.Uri.OriginalString);
+        Assert.IsTrue(file.Uri.OriginalString.Contains(file.Name));
+        Assert.AreEqual(file.Name, "test.zip");
+    }
 
-            Assert.AreNotEqual(file.Name, file.Uri.OriginalString);
-            Assert.IsTrue(file.Uri.OriginalString.Contains(file.Name));
-            Assert.AreEqual(file.Name, "test.zip");
-        }
+    [TestMethod]
+    public void FileNameOnUrlWithParams()
+    {
+        var file = new HttpFile(new Uri("https://example.com/test.zip?utm_content=This+Is+\"Broken\""));
 
-        [TestMethod]
-        public void FileNameOnUrlWithParams()
-        {
-            var file = new HttpFile(new Uri("https://example.com/test.zip?utm_content=This+Is+\"Broken\""));
-
-            Assert.AreNotEqual(file.Name, file.Uri.OriginalString);
-            Assert.IsTrue(file.Uri.OriginalString.Contains(file.Name));
-            Assert.AreEqual(file.Name, "test.zip");
-        }
+        Assert.AreNotEqual(file.Name, file.Uri.OriginalString);
+        Assert.IsTrue(file.Uri.OriginalString.Contains(file.Name));
+        Assert.AreEqual(file.Name, "test.zip");
     }
 }
