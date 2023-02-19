82da6e5bb3691c4c834b24bed5a986ab4f0a596e
Arlo <arlo.godfrey@outlook.com>
2023-02-18 18:12:18 -0600

    Documentation tweaks

--- src/IStorageProperty.cs
+++ src/IStorageProperty.cs
@@ -6,7 +6,7 @@ namespace OwlCore.Storage;
 /// A property that was retrieved asynchronously, and can be disposed. The <paramref see="Value"/> can be a primitive or an object containing multiple values.
 /// </summary>
 /// <remarks>
-/// The recommended pattern for properties is create an async method that returns <c>IStorageProperty{SomePropertyType}</c>, put that method on an interface, and use the interface to indicate support for this property value.
+/// The recommended pattern for properties is create an async method that returns <c>IStorageProperty{SomePropertyType}</c>, put that method on an interface, and use the interface to indicate optional support for this property value.
 /// </remarks>
 /// <typeparam name="T">The type used to store information about properties.</typeparam>
 public interface IStorageProperty<T> : IDisposable
