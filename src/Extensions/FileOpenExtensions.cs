2bcf20ec6cc7a16700b7e22ff40b56bff38612ef
Arlo Godfrey <arlo.godfrey@outlook.com>
2024-03-09 13:55:27 -0600

    Removed default FileAccess.Read parameter for IFile.OpenStreamAsync

---
+++ src/Extensions/FileOpenExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OwlCore.Storage.Extensions;
+
+/// <summary>
+/// Extension methods for <see cref="IFile"/>.
+/// </summary>
+public static partial class FileExtensions
+{
+    /// <summary>
+    /// Opens the file for reading.
+    /// </summary>
+    /// <param name="file">The file to open.</param>
+    /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
+    /// <returns>A task containing the requested stream.</returns>
+    public static Task<Stream> OpenReadAsync(this IFile file, CancellationToken cancellationToken = default) => file.OpenStreamAsync(FileAccess.Read, cancellationToken);
+    
+    /// <summary>
+    /// Opens the file for writing.
+    /// </summary>
+    /// <param name="file">The file to open.</param>
+    /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
+    /// <returns>A task containing the requested stream.</returns>
+    public static Task<Stream> OpenWriteAsync(this IFile file, CancellationToken cancellationToken = default) => file.OpenStreamAsync(FileAccess.Write, cancellationToken);
+    
+    /// <summary>
+    /// Opens the file for reading and writing.
+    /// </summary>
+    /// <param name="file">The file to open.</param>
+    /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
+    /// <returns>A task containing the requested stream.</returns>
+    public static Task<Stream> OpenReadWriteAsync(this IFile file, CancellationToken cancellationToken = default) => file.OpenStreamAsync(FileAccess.ReadWrite, cancellationToken);
+}
--- src/IFile.cs
+++ src/IFile.cs
@@ -15,5 +15,5 @@ public interface IFile : IStorable
     /// <param name="accessMode">A <see cref="FileAccess"/> value that specifies the operations that can be performed on the file.</param>
     /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
     /// <returns>A stream that provides access to this file, with the specified <paramref name="accessMode"/>.</returns>
-    Task<Stream> OpenStreamAsync(FileAccess accessMode = FileAccess.Read, CancellationToken cancellationToken = default);
+    Task<Stream> OpenStreamAsync(FileAccess accessMode, CancellationToken cancellationToken = default);
 }
