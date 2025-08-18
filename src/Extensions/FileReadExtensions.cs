65d0b3e2c597932bb5dbec8e30c0c3a58f47fc74
Arlo <arlo.godfrey@outlook.com>
2025-08-17 20:36:08 -0500

    Made ranged ReadTextAsync EOF-safe and documented [Start, End] semantics

--- src/Extensions/FileReadExtensions.cs
+++ src/Extensions/FileReadExtensions.cs
@@ -16,25 +16,16 @@ public static class FileReadExtensions
     /// <summary>
     /// Opens the file for reading and reads the content into a byte array.
     /// </summary>
-    /// <param name="file">The file to read.</param>
-    /// <returns>A Task containing a byte array of the file contents.</returns>
     public static Task<byte[]> ReadBytesAsync(this IFile file) => file.ReadBytesAsync(default);
 
     /// <summary>
     /// Opens the file for reading and reads the content into a byte array.
     /// </summary>
-    /// <param name="file">The file to read.</param>
-    /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
-    /// <returns>A Task containing a byte array of the file contents.</returns>
     public static Task<byte[]> ReadBytesAsync(this IFile file, CancellationToken cancellationToken = default) => file.ReadBytesAsync(bufferSize: 81920, cancellationToken);
 
     /// <summary>
     /// Opens the file for reading and reads the content into a byte array.
     /// </summary>
-    /// <param name="file">The file to read from.</param>
-    /// <param name="bufferSize">The size, in bytes, of the buffer. This value must be greater than zero. The default size is 81920.</param>
-    /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
-    /// <returns>A Task containing a byte array of the file contents.</returns>
     public static async Task<byte[]> ReadBytesAsync(this IFile file, int bufferSize = 81920, CancellationToken cancellationToken = default)
     {
         using var stream = await file.OpenReadAsync(cancellationToken);
@@ -44,27 +35,27 @@ public static class FileReadExtensions
     }
 
     /// <summary>
-    /// Opens the file for reading and reads the content into a byte array.
+    /// Opens the file for reading and reads the content into a string using UTF-8 encoding.
     /// </summary>
     /// <param name="file">The file to read from.</param>
-    /// <returns>A Task containing a byte array of the file contents.</returns>
+    /// <returns>A task containing the string content of the file.</returns>
     public static Task<string> ReadTextAsync(this IFile file) => file.ReadTextAsync(default);
 
     /// <summary>
-    /// Opens the file for reading and reads the content into a byte array.
+    /// Opens the file for reading and reads the content into a string using UTF-8 encoding.
     /// </summary>
     /// <param name="file">The file to read from.</param>
     /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
-    /// <returns>A Task containing a byte array of the file contents.</returns>
+    /// <returns>A task containing the string content of the file.</returns>
     public static Task<string> ReadTextAsync(this IFile file, CancellationToken cancellationToken = default) => file.ReadTextAsync(Encoding.UTF8, cancellationToken);
 
     /// <summary>
-    /// Opens the file for reading and reads the content into a byte array.
+    /// Opens the file for reading and reads the content into a string using UTF-8 encoding.
     /// </summary>
     /// <param name="file">The file to read from.</param>
-    /// <param name="bufferSize">The size, in bytes, of the buffer. This value must be greater than zero. The default size is 81920.</param>
+    /// <param name="bufferSize">The size, in bytes, of the buffer used when reading. The default size is 81920.</param>
     /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
-    /// <returns>A Task containing a byte array of the file contents.</returns>
+    /// <returns>A task containing the string content of the file.</returns>
     public static async Task<string> ReadTextAsync(this IFile file, int bufferSize = 81920, CancellationToken cancellationToken = default)
     {
         var bytes = await file.ReadBytesAsync(bufferSize, cancellationToken);
@@ -72,22 +63,22 @@ public static class FileReadExtensions
     }
 
     /// <summary>
-    /// Opens the file for reading and reads the content into a byte array.
+    /// Opens the file for reading and reads the content into a string using the specified encoding.
     /// </summary>
     /// <param name="file">The file to read from.</param>
     /// <param name="encoding">The encoding to use for reading text from the file.</param>
     /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
-    /// <returns>A Task containing a byte array of the file contents.</returns>
+    /// <returns>A task containing the string content of the file.</returns>
     public static Task<string> ReadTextAsync(this IFile file, Encoding encoding, CancellationToken cancellationToken = default) => file.ReadTextAsync(encoding, 81920, cancellationToken: cancellationToken);
 
     /// <summary>
-    /// Opens the file for reading and reads the content into a byte array.
+    /// Opens the file for reading and reads the content into a string using the specified encoding.
     /// </summary>
     /// <param name="file">The file to read from.</param>
     /// <param name="encoding">The encoding to use for reading text from the file.</param>
-    /// <param name="bufferSize">The size, in bytes, of the buffer. This value must be greater than zero. The default size is 81920.</param>
+    /// <param name="bufferSize">The size, in bytes, of the buffer used when reading. The default size is 81920.</param>
     /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
-    /// <returns>A Task containing a byte array of the file contents.</returns>
+    /// <returns>A task containing the string content of the file.</returns>
     public static async Task<string> ReadTextAsync(this IFile file, Encoding encoding, int bufferSize = 81920, CancellationToken cancellationToken = default)
     {
         var bytes = await file.ReadBytesAsync(bufferSize, cancellationToken);
@@ -101,6 +92,14 @@ public static class FileReadExtensions
     /// <param name="lineRange">The line range to read.</param>
     /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
     /// <returns>A per-line async enumerable of the read content.</returns>
+    /// <summary>
+    /// Reads a specific line range from the text file.
+    /// </summary>
+    /// <remarks>
+    /// - Lines are 0-based indexed.
+    /// - <c>End</c> is exclusive; the sequence yields lines in [<c>Start</c>, <c>End</c>).
+    /// - If EOF is reached before <c>Start</c> or during enumeration, the sequence ends early without error.
+    /// </remarks>
 #if NETSTANDARD
     public static async IAsyncEnumerable<string> ReadTextAsync(this IFile sourceFile, (int Start, int End) lineRange, [EnumeratorCancellation] CancellationToken cancellationToken)
 #elif NET7_OR_GREATER
@@ -110,36 +109,38 @@ public static class FileReadExtensions
         using var fileStream = await sourceFile.OpenReadAsync(cancellationToken);
         using var streamReader = new StreamReader(fileStream);
 
-        // Fast-forward to range start
-        var currentLine = 0;
 #if NETSTANDARD
-        while (currentLine++ < lineRange.Start)
+        var start = lineRange.Start;
+        var end = lineRange.End;
 #elif NET7_OR_GREATER
-        while (currentLine++ < lineRange.Start.Value)
+        var start = lineRange.Start.Value;
+        var end = lineRange.End.Value;
 #endif
 
+        for (var i = 0; i < start; i++)
         {
 #if NETSTANDARD
-            _ = await streamReader.ReadLineAsync();
+            cancellationToken.ThrowIfCancellationRequested();
+            if (await streamReader.ReadLineAsync() is null)
+                yield break;
 #elif NET7_OR_GREATER
-            _ = await streamReader.ReadLineAsync(cancellationToken);
+            if (await streamReader.ReadLineAsync(cancellationToken) is null)
+                yield break;
 #endif
         }
 
-        // Read to range end
-#if NETSTANDARD
-        while (currentLine++ <= lineRange.End)
-#elif NET7_OR_GREATER
-        while (currentLine++ <= lineRange.End.Value)
-#endif
+        for (var i = start; i < end; i++)
         {
 #if NETSTANDARD
+            cancellationToken.ThrowIfCancellationRequested();
             var line = await streamReader.ReadLineAsync();
 #elif NET7_OR_GREATER
             var line = await streamReader.ReadLineAsync(cancellationToken);
 #endif
-            if (line is not null)
-                yield return line;
+            if (line is null)
+                yield break;
+
+            yield return line;
         }
     }
 
@@ -151,6 +152,15 @@ public static class FileReadExtensions
     /// <param name="columnRange">The character range within each line to return.</param>
     /// <param name="cancellationToken">A token that can be used to cancel the ongoing operation.</param>
     /// <returns>A per-line async enumerable of the read content.</returns>
+    /// <summary>
+    /// Reads a specific column range from each line within a line range.
+    /// </summary>
+    /// <remarks>
+    /// - Lines and columns are 0-based indexed.
+    /// - Both <c>lineRange.End</c> and <c>columnRange.End</c> are exclusive; lines and columns are taken from [Start, End).
+    /// - If EOF is reached before <c>lineRange.Start</c> or during enumeration, the sequence ends early without error.
+    /// - If <c>columnRange.Start</c> is beyond the end of a line, an empty string is yielded for that line.
+    /// </remarks>
 #if NETSTANDARD
     public static async IAsyncEnumerable<string> ReadTextAsync(this IFile sourceFile, (int Start, int End) lineRange, (int Start, int End) columnRange, [EnumeratorCancellation] CancellationToken cancellationToken)
 #elif NET7_OR_GREATER
@@ -160,45 +170,51 @@ public static class FileReadExtensions
         using var fileStream = await sourceFile.OpenReadAsync(cancellationToken);
         using var streamReader = new StreamReader(fileStream);
 
-        // Skip to line range start
-        var currentLine = 0;
 #if NETSTANDARD
-        while (currentLine++ < lineRange.Start)
-            _ = await streamReader.ReadLineAsync();
+        var start = lineRange.Start;
+        var end = lineRange.End;
+        var colStart = columnRange.Start;
+        var colEnd = columnRange.End;
 #elif NET7_OR_GREATER
-        while (currentLine++ < lineRange.Start.Value)
-            _ = await streamReader.ReadLineAsync(cancellationToken);
+        var start = lineRange.Start.Value;
+        var end = lineRange.End.Value;
+        var colStart = columnRange.Start.Value;
+        var colEnd = columnRange.End.Value;
 #endif
 
-        // Take to line range end
-#if NETSTANDARD
-    while (currentLine++ <= lineRange.End)
+        for (var i = 0; i < start; i++)
         {
-            var line = await streamReader.ReadLineAsync();
-            if (line is not null)
-            {
-                var start = columnRange.Start;
-                var end = columnRange.End;
-                var length = Math.Max(0, end - start);
+#if NETSTANDARD
+            cancellationToken.ThrowIfCancellationRequested();
+            if (await streamReader.ReadLineAsync() is null)
+                yield break;
+#elif NET7_OR_GREATER
+            if (await streamReader.ReadLineAsync(cancellationToken) is null)
+                yield break;
+#endif
+        }
 
-                if (start >= line.Length)
-                {
-                    yield return string.Empty;
-                }
-                else
-                {
-                    var safeLen = Math.Min(length, line.Length - start);
-                    yield return line.Substring(start, safeLen);
-                }
+        for (var i = start; i < end; i++)
+        {
+#if NETSTANDARD
+            cancellationToken.ThrowIfCancellationRequested();
+            var line = await streamReader.ReadLineAsync();
+#elif NET7_OR_GREATER
+            var line = await streamReader.ReadLineAsync(cancellationToken);
+#endif
+            if (line is null)
+                yield break;
+
+            var length = Math.Max(0, colEnd - colStart);
+            if (colStart >= line.Length)
+            {
+                yield return string.Empty;
+            }
+            else
+            {
+                var safeLen = Math.Min(length, line.Length - colStart);
+                yield return line.Substring(colStart, safeLen);
             }
         }
-#elif NET7_OR_GREATER
-        while (currentLine++ <= lineRange.End.Value)
-        {
-            var line = await streamReader.ReadLineAsync(cancellationToken);
-            if (line is not null)
-                yield return new([.. line.Skip(columnRange.Start.Value).Take(columnRange.End.Value - columnRange.Start.Value)]);
-        }
-#endif
     }
 }
--- tests/OwlCore.Storage.Tests/FileRangeReadWriteTests.cs
+++ tests/OwlCore.Storage.Tests/FileRangeReadWriteTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -9,8 +10,8 @@ namespace OwlCore.Storage.Tests;
 [TestClass]
 public class FileRangeReadWriteTests
 {
-        private static Task<IModifiableFolder> CreateRootAsync()
-                => Task.FromResult((IModifiableFolder)new MemoryFolder("root-id", "root"));
+    private static Task<IModifiableFolder> CreateRootAsync()
+            => Task.FromResult((IModifiableFolder)new MemoryFolder("root-id", "root"));
 
     [TestMethod]
     public async Task Read_Lines_Range_AllColumns()
@@ -21,7 +22,7 @@ public class FileRangeReadWriteTests
         var content = string.Join("\n", new[] { "L0", "L1", "L2", "L3", "L4" });
         await file.WriteTextAsync(content, Encoding.UTF8);
 
-            var lines = await file.ReadTextAsync((1, 4), CancellationToken.None).ToListAsync();
+        var lines = await file.ReadTextAsync((1, 4), CancellationToken.None).ToListAsync();
         CollectionAssert.AreEqual(new[] { "L1", "L2", "L3" }, lines);
     }
 
@@ -34,18 +35,18 @@ public class FileRangeReadWriteTests
         var content = string.Join("\n", new[] { "abcd", "efgh", "ijkl", "mnop" });
         await file.WriteTextAsync(content, Encoding.UTF8);
 
-            var lines = await file.ReadTextAsync((1, 3), (1, 3), CancellationToken.None).ToListAsync();
+        var lines = await file.ReadTextAsync((1, 3), (1, 3), CancellationToken.None).ToListAsync();
         CollectionAssert.AreEqual(new[] { "fg", "jk" }, lines);
     }
 
     [TestMethod]
     public async Task Write_Lines_Range_AllColumns()
     {
-    var root = await CreateRootAsync();
-    var dst = (IFile)await root.CreateFileAsync("dst.txt");
+        var root = await CreateRootAsync();
+        var dst = (IFile)await root.CreateFileAsync("dst.txt");
 
-    var content = string.Join("\n", new[] { "A0", "A1", "A2", "A3" });
-    await dst.WriteTextAsync(content, (1, 3), CancellationToken.None);
+        var content = string.Join("\n", new[] { "A0", "A1", "A2", "A3" });
+        await dst.WriteTextAsync(content, (1, 3), CancellationToken.None);
         var output = await dst.ReadTextAsync();
         Assert.AreEqual("A1\nA2\n", output.Replace("\r\n", "\n"));
     }
@@ -53,12 +54,38 @@ public class FileRangeReadWriteTests
     [TestMethod]
     public async Task Write_Lines_And_Columns_Range()
     {
-    var root = await CreateRootAsync();
-    var dst = (IFile)await root.CreateFileAsync("dst.txt");
+        var root = await CreateRootAsync();
+        var dst = (IFile)await root.CreateFileAsync("dst.txt");
 
-    var content = string.Join("\n", new[] { "abcd", "efgh", "ijkl", "mnop" });
-    await dst.WriteTextAsync(content, (1, 4), (1, 3), CancellationToken.None);
+        var content = string.Join("\n", new[] { "abcd", "efgh", "ijkl", "mnop" });
+        await dst.WriteTextAsync(content, (1, 4), (1, 3), CancellationToken.None);
         var lines = await dst.ReadTextAsync().ConfigureAwait(false);
-    Assert.AreEqual("fg\njk\nno\n", lines.Replace("\r\n", "\n"));
+        Assert.AreEqual("fg\njk\nno\n", lines.Replace("\r\n", "\n"));
+    }
+
+    [TestMethod]
+    public async Task Read_Lines_Range_Completely_Beyond_EOF_Returns_Empty()
+    {
+        var root = await CreateRootAsync();
+        var file = (IFile)await root.CreateFileAsync("eof.txt");
+
+        await file.WriteTextAsync("L0\nL1", Encoding.UTF8);
+
+        // File has 2 lines (0,1). Request [5,8) should produce no output and must not hang.
+        var lines = await file.ReadTextAsync((5, 8), CancellationToken.None).ToListAsync();
+        Assert.AreEqual(0, lines.Count);
+    }
+
+    [TestMethod]
+    public async Task Read_Lines_Range_Partially_Beyond_EOF_Yields_Remaining_Lines_Only()
+    {
+        var root = await CreateRootAsync();
+        var file = (IFile)await root.CreateFileAsync("partial.txt");
+
+        await file.WriteTextAsync("A\nB\nC", Encoding.UTF8); // indices 0,1,2
+
+        // Request [1, 5): should yield B (1) and C (2), then stop at EOF.
+        var lines = await file.ReadTextAsync((1, 5), CancellationToken.None).ToListAsync();
+        CollectionAssert.AreEqual(new[] { "B", "C" }, lines);
     }
 }
